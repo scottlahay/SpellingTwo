99e35a712eef5f9db77e4fb4d4eb6755bca62a68
scottlahay <scott.lahay.wave@gmail.com>
2016-10-30 07:48:28 -0700

    Working through v3

--- app/src/main/java/scott/spellingtwo/ui/PracticeActivity.java
+++ app/src/main/java/scott/spellingtwo/ui/PracticeActivity.java
@@ -1,43 +1,45 @@
 package scott.spellingtwo.ui;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.graphics.Color;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.view.Gravity;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.TextSwitcher;
-import android.widget.TextView;
-import android.widget.ViewSwitcher;
+import android.app.*;
+import android.content.*;
+import android.graphics.*;
+import android.os.*;
+import android.speech.tts.*;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.*;
+import android.view.*;
+import android.widget.*;
 
-import scott.spellingtwo.R;
-import scott.spellingtwo.domain.SpellingList;
-import scott.spellingtwo.domain.SpellingLists;
+import butterknife.*;
+import scott.spellingtwo.*;
+import scott.spellingtwo.domain.*;
+
+import static android.R.anim.*;
+import static android.R.id.*;
+import static android.view.Gravity.*;
+import static android.view.Window.*;
+import static android.view.WindowManager.LayoutParams.*;
+import static android.view.animation.AnimationUtils.*;
+import static scott.spellingtwo.R.id.*;
+import static scott.spellingtwo.R.style.*;
 
 public class PracticeActivity extends Activity {
-    private String answerText = "";
-    private TextSwitcher switcher;
-    private TextToSpeech speak;
-    private SpellingLists lists;
-    private SpellingList list;
-    private String[] listNames;
-    private ProgressDialog progress;
+    @BindView(swtAnswer_p) TextSwitcher swtAnswer;
+    ProgressDialog progress;
+
+    String answerText = "";
+    TextToSpeech speak;
+    SpellingLists lists;
+    SpellingList list;
+    String[] listNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(FEATURE_NO_TITLE);
+        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
         setContentView(R.layout.activity_spelling_two);
+        ButterKnife.bind(this);
         UpdateSpellingListsFromGoogleSheets updating = new UpdateSpellingListsFromGoogleSheets();
         updating.execute();
         waitForTheProgramToLoad();
@@ -45,7 +47,6 @@ public class PracticeActivity extends Activity {
         while (!updating.done) {
             try {
                 Thread.sleep(1000);
-
             } catch (InterruptedException e) {
                 // not concerned if this blows chunkies
                 System.out.println("thread bailed");
@@ -53,7 +54,6 @@ public class PracticeActivity extends Activity {
         }
         progress.dismiss();
         haveTheUserChooseHisList();
-
     }
 
     public void waitForTheProgramToLoad() {
@@ -68,14 +68,15 @@ public class PracticeActivity extends Activity {
         listNames = lists.getAllListNames();
         textSwitchStuff();
         popUpChooseList();
+        getWindow().setSoftInputMode(SOFT_INPUT_STATE_VISIBLE);
     }
 
     private void updateHeader() {
-        ((TextView) findViewById(R.id.completionLabel)).setText(list.current + "/" + list.size());
+        ((TextView) findViewById(txtCompletion)).setText(list.current + "/" + list.size());
     }
 
     public void popUpChooseList() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, MyAlertDialogStyle);
         builder.setTitle("Pick your list");
         builder.setItems(listNames, new DialogInterface.OnClickListener() {
             @Override
@@ -87,26 +88,22 @@ public class PracticeActivity extends Activity {
         builder.show();
     }
 
-    private void popUpCurrentWord() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(list.currentWord());
-        builder.setTitle("The word is");
-        AlertDialog dialog = builder.show();
-        TextView view = (TextView) dialog.findViewById(android.R.id.message);
-        view.setGravity(Gravity.CENTER);
-    }
+    private void popUpCurrentWord() { buildDialog("The word is"); }
 
-    private void popUpStartingWord() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    private void popUpStartingWord() { buildDialog("Let's get started with ");}
+
+    private void buildDialog(String title) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, MyAlertDialogStyle);
         builder.setMessage(list.currentWord());
-        builder.setTitle("Let's get started with ");
-        AlertDialog dialog = builder.show();
-        TextView view = (TextView) dialog.findViewById(android.R.id.message);
-        view.setGravity(Gravity.CENTER);
+        builder.setTitle(title);
+        builder.setPositiveButton("OK", null);
+        AppCompatDialog dialog = builder.show();
+        TextView view = (TextView) dialog.findViewById(message);
+        view.setGravity(CENTER);
     }
 
     private void popUpYouFinished() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this,MyAlertDialogStyle);
         builder.setTitle("Finished!");
         builder.setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
             @Override
@@ -116,15 +113,10 @@ public class PracticeActivity extends Activity {
         }).setNegativeButton("Exit", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                exit(null);
+                finish();
             }
         });
         builder.show();
-    }
-
-    public void exit(View view) {
-        android.os.Process.killProcess(android.os.Process.myPid());
-        System.exit(1);
     }
 
     public void reset() {
@@ -135,25 +127,25 @@ public class PracticeActivity extends Activity {
     }
 
     public void textSwitchStuff() {
-        switcher = (TextSwitcher) findViewById(R.id.answerSwitcher);
-        switcher.setFactory(new ViewSwitcher.ViewFactory() {
+        swtAnswer = (TextSwitcher) findViewById(swtAnswer_p);
+        swtAnswer.setFactory(new ViewSwitcher.ViewFactory() {
             public View makeView() {
                 TextView myText = new TextView(PracticeActivity.this);
-                myText.setGravity(Gravity.CENTER);
+                myText.setGravity(CENTER);
                 myText.setTextSize(100);
                 myText.setTextColor(Color.BLACK);
                 myText.setTextIsSelectable(false);
                 return myText;
             }
         });
-        Animation in = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
-        Animation out = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
-        switcher.setInAnimation(in);
-        switcher.setOutAnimation(out);
+        swtAnswer.setInAnimation(loadAnimation(this, slide_in_left));
+        swtAnswer.setOutAnimation(loadAnimation(this, slide_out_right));
     }
 
-    public void userButtonPress(View v) {
-        answerText += ((Button) v).getText().toString();
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {return true;}
+        answerText += event.getDisplayLabel();
         updateText();
         if (list.isCorrectAnswer(answerText)) {
             if (list.finished()) {
@@ -165,10 +157,11 @@ public class PracticeActivity extends Activity {
                 hint(null);
             }
         }
+        return super.dispatchKeyEvent(event);
     }
 
     private void updateText() {
-        switcher.setCurrentText(answerText);
+        swtAnswer.setCurrentText(answerText);
         updateHeader();
     }
 
@@ -179,7 +172,7 @@ public class PracticeActivity extends Activity {
 
     public void clearTheWord(View v) {
         answerText = "";
-        switcher.setText(answerText);
+        swtAnswer.setText(answerText);
     }
 
     @Override
