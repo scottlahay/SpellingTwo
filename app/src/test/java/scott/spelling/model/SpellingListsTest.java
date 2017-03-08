8ee45db2f761da5e737f37c3fc54cc7ae5d8fb10
scottlahay <scott.lahay.wave@gmail.com>
2017-03-08 12:04:33 -0800

    finished moving the logic from the activity to the presenter

---
+++ app/src/test/java/scott/spelling/model/SpellingListsTest.java
@@ -0,0 +1,29 @@
+package scott.spelling.model;
+
+import org.junit.*;
+
+import scott.spelling.system.*;
+
+import static org.junit.Assert.*;
+import static scott.spelling.system.TestUtils.*;
+
+public class SpellingListsTest {
+
+    SpellingLists lists;
+
+    @Before
+    public void before() {
+        lists = TestUtils.spellingLists();
+    }
+
+    @Test
+    public void spellingListsAreEmptyWhenTheDontContainAList() throws Throwable {
+        assertTrue(emptySpellingLists().empty());
+        assertFalse(spellingLists().empty());
+    }
+
+    @Test
+    public void aListCanBeFoundByItsIndexNumber() throws Throwable {
+        assertEquals(spellingList(), lists.findList(0));
+    }
+}
